c9a428f1c0eae5b3280f37cdae3a162dff4b4a0f
A-Khoobi <87467061+a-khoobi@users.noreply.github.com>
2021-12-26 09:24:28 +0330

    initial

---
+++ src/main/java/ir/khoobi/jirasynchronizer/JiraSynchronizerApplication.java
@@ -0,0 +1,13 @@
+package ir.khoobi.jirasynchronizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraSynchronizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiraSynchronizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/khoobi/jirasynchronizer/JiraSynchronizerApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.khoobi.jirasynchronizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraSynchronizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
